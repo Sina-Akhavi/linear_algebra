8ae5d01f0b7f871526558a5f5f3ce63769a575e0
Sina Akhavi <SinaAkhaviSpecial1984@gmail.com>
2023-10-27 11:06:40 +0330

    modified task3

--- lab04/task3/task3.py
+++ lab04/task3/task3.py
@@ -9,8 +9,11 @@ I = imageio.imread('nasir-al-mulk-gray.jpg')
 # I_reversed_tiled = np.tile(I_reversed, 2)
 # I_output = np.vstack((I_tiled, I_reversed_tiled))
 
-I_output = np.vstack((np.tile(I, 2), np.tile(np.flip(I, axis=0), 2)))  # solution
+# I_output = np.vstack((np.tile(I, 2), np.tile(np.flip(I, axis=0), 2)))  # solution
 
+# Mr.Godarzi solution
+P1 = np.concatenate((I, I[:, ::-1]), axis=1)
+I_output = np.concatenate((P1, P1[::-1, :]))
 plt.imshow(I_output, cmap='gray')
 
 plt.show()
